d54068883d9a864f92effa89b2ba4d0d5fce525f
Yan Khachko <a@slnk.icu>
2021-10-09 21:32:30 +0100

    micro fix for new modes.py format

--- modes.py
+++ modes.py
@@ -11,7 +11,7 @@ default = {
 
 
 def margin(logo_size, img_size, scale):
-    return img_size[0]*0.02
+    return img_size[0] * 0.02
 
 
 def right(logo_size, img_size, scale):
@@ -28,46 +28,52 @@ def top(logo_size, img_size, scale):
 
 def bottom(logo_size, img_size, scale):
     return int(img_size[1] - int(logo_size[1] *
-                             get_new_logo_size(logo_size, img_size, scale) / logo_size[0])
+                                 get_new_logo_size(logo_size, img_size, scale) / logo_size[0])
                - margin(logo_size, img_size, scale))
 
 
 mode_for_langs = {
-        'right_bottom':
-            {
-                'x': right,
-                'y': bottom,
-                'scale': get_new_logo_size,
-                'opacity': lambda _, __, ___: 0.8,
-                'button_text': '⌟'
-            },
-        'left_bottom':
-            {
-                'x': left,
-                'y': bottom,
-                'scale': get_new_logo_size,
-                'opacity': lambda _, __, ___: 0.8,
-                'button_text': '⌞'
-            },
-        'left_top':
-            {
-                'x': left,
-                'y': top,
-                'scale': get_new_logo_size,
-                'opacity': lambda _, __, ___: 0.8,
-                'button_text': '⌜'
-            },
-        'right_top':
-            {
-                'x': right,
-                'y': top,
-                'scale': get_new_logo_size,
-                'opacity': lambda _, __, ___: 0.8,
-                'button_text': '⌝'
-            }
-    }
+    'right_bottom':
+        {
+            'x': right,
+            'y': bottom,
+            'scale': get_new_logo_size,
+            'opacity': lambda _, __, ___: 0.8,
+            'button_text': '⌟'
+        },
+    'left_bottom':
+        {
+            'x': left,
+            'y': bottom,
+            'scale': get_new_logo_size,
+            'opacity': lambda _, __, ___: 0.8,
+            'button_text': '⌞'
+        },
+    'left_top':
+        {
+            'x': left,
+            'y': top,
+            'scale': get_new_logo_size,
+            'opacity': lambda _, __, ___: 0.8,
+            'button_text': '⌜'
+        },
+    'right_top':
+        {
+            'x': right,
+            'y': top,
+            'scale': get_new_logo_size,
+            'opacity': lambda _, __, ___: 0.8,
+            'button_text': '⌝'
+        }
+}
 
 modes = {
-    'rus': mode_for_langs,
-    'eng': mode_for_langs
+    'rus': {
+        'mode': mode_for_langs,
+        'button_text': 'Русский'
+    },
+    'eng': {
+        'mode': mode_for_langs,
+        'button_text': 'Английский'
+    }
 }
